4b8274111d3f1c399b7ce8cdac66c36cc9dba643
Manfred Touron <m@42.am>
2015-08-26 01:37:28 +0200

    Using two independent routines

--- twitter-redis.go
+++ twitter-redis.go
@@ -2,7 +2,6 @@ package twitterredis
 
 import (
 	"encoding/json"
-	"fmt"
 	"time"
 
 	"github.com/Sirupsen/logrus"
@@ -16,40 +15,77 @@ type Message struct {
 	Author  string `json:"author"`
 }
 
+type Server struct {
+	TwitterClient *twitterstream.Client
+	RedisClient   *redis.Client
+	Track         string
+	Messages      chan twitterstream.Tweet
+	Done          chan bool
+}
+
 func ServerAction(c *cli.Context) {
-	twitterClient := twitterstream.NewClient(c.String("twitter-consumer-key"), c.String("twitter-consumer-secret"), c.String("twitter-access-token"), c.String("twitter-access-secret"))
+	server := Server{
+		TwitterClient: twitterstream.NewClient(c.String("twitter-consumer-key"), c.String("twitter-consumer-secret"), c.String("twitter-access-token"), c.String("twitter-access-secret")),
+		RedisClient: redis.NewClient(&redis.Options{
+			Addr:       c.String("redis-server"),
+			Password:   c.String("redis-password"),
+			DB:         0,
+			MaxRetries: 1000,
+		}),
+		Track:    c.String("twitter-track"),
+		Messages: make(chan twitterstream.Tweet, 42),
+		Done:     make(chan bool, 1),
+	}
+	defer server.RedisClient.Close()
 
-	redisClient := redis.NewClient(&redis.Options{
-		Addr:     c.String("redis-server"),
-		Password: c.String("redis-password"),
-		DB:       0,
-	})
+	logrus.Infof("Starting ReadFromStream routine...")
+	go server.ReadFromStream()
+	logrus.Infof("Starting WriteToRedis routine...")
+	go server.WriteToRedis()
 
+	<-server.Done
+	logrus.Fatalf("Done received")
+}
+
+func (s *Server) ReadFromStream() {
 	for {
-		conn, err := twitterClient.Track(c.String("twitter-track"))
+		// (re)connect
+		logrus.Infof("Connecting to stream with track=%q", s.Track)
+		conn, err := s.TwitterClient.Track(s.Track)
 		if err != nil {
-			logrus.Warnf("Disconnected, waiting for 5 second: %v", err)
+			logrus.Errorf("Disconnected, waiting for 5 second: %v", err)
 			time.Sleep(5 * time.Second)
 			continue
 		}
 
 		for {
 			if tweet, err := conn.Next(); err == nil {
-				message := Message{
-					Message: tweet.Text,
-					Author:  tweet.User.ScreenName,
-				}
-				fmt.Printf("%s: %q\n", message.Author, message.Message)
-				b, err := json.Marshal(message)
-				if err != nil {
-					logrus.Warnf("json.Marshal error: %v", err)
-					continue
-				}
-				redisClient.LPush("messages.generic", string(b))
+				logrus.Infof("Received tweet from %q: %q", tweet.User.ScreenName, tweet.Text)
+				s.Messages <- *tweet
 			} else {
-				logrus.Warnf("No next tweet: %v", err)
+				logrus.Warnf("Read stream error, try to reconnect: %v", err)
 				break
 			}
+		}
+	}
+}
+
+func (s *Server) WriteToRedis() {
+	for tweet := range s.Messages {
+		message := Message{
+			Message: tweet.Text,
+			Author:  tweet.User.ScreenName,
+		}
+		// fmt.Printf("%s: %q\n", message.Author, message.Message)
+		b, err := json.Marshal(message)
+		if err != nil {
+			logrus.Warnf("json.Marshal error: %v", err)
+			continue
+		}
+		err = s.RedisClient.LPush("messages.generic", string(b)).Err()
+		if err != nil {
+			logrus.Errorf("Failed to write to redis")
+			// reconnect ? seems to be done by redisclient using MaxRetries
 		}
 	}
 }
